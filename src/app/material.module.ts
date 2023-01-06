40a95d6cced8cd1520183315cf5e9bbae26d6920
Eldest Pasirula <aknutman@gmail.com>
2023-01-06 16:07:54 +0000

    display table and filter

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatTableModule,
     MatToolbarModule,
   ],
   exports: [
     MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatTableModule,
     MatToolbarModule,
   ],
 })
