beff4d481fac27ce52f04215c20e27cc0a9d33f0
Eldest Pasirula <aknutman@gmail.com>
2023-01-07 15:48:25 +0000

    displayed as table with dummy data

--- src/app/main-panel/as-table/as-table.component.ts
+++ src/app/main-panel/as-table/as-table.component.ts
@@ -1,57 +1,38 @@
-import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { Subscription } from 'rxjs';
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 12, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 15, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 16, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 17, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 18, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 19, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 20, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 21, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 22, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 23, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 24, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 25, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 26, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 27, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 28, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 29, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 30, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+import { MainPanelTableModel } from '../../../model/root/main-panel/property';
+import { PropertyService } from '../../../services/main-panel/property.service';
 
 @Component({
   selector: 'app-as-table',
   templateUrl: './as-table.component.html',
   styleUrls: ['./as-table.component.css'],
 })
-export class AsTableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class AsTableComponent implements OnInit, OnDestroy {
+  tableServiceSub: Subscription = new Subscription();
+
+  tableData: MainPanelTableModel | undefined;
+
+  constructor(private tableService: PropertyService) {}
+
+  ngOnInit(): void {
+    this.tableServiceSub = this.tableService
+      .getRootSidebarProperty()
+      .subscribe((item: MainPanelTableModel) => {
+        this.tableData = item;
+        // console.log('item:', item);
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.tableServiceSub) {
+      this.tableServiceSub.unsubscribe();
+    }
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
--- src/app/service.module.ts
+++ src/app/service.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { PropertyService as RootLayoutProperty } from '../services/root/layout/property.service';
 import { PropertyService as SidebarPropertyService } from '../services/root/sidebar/property.service';
+import { PropertyService as TableService } from '../services/main-panel/property.service';
 
 @NgModule({
-  providers: [RootLayoutProperty, SidebarPropertyService],
+  providers: [RootLayoutProperty, SidebarPropertyService, TableService],
 })
 export class ServiceModule {}
