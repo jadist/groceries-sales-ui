31c4ac35e789e3b8f774f93af907f1168eb1d655
Eldest Pasirula <aknutman@gmail.com>
2023-01-07 16:53:03 +0000

    ui of paginator and padding

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -19,6 +20,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatPaginatorModule,
     MatTableModule,
     MatToolbarModule,
   ],
@@ -30,6 +32,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatPaginatorModule,
     MatTableModule,
     MatToolbarModule,
   ],
