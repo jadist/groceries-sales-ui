e40990e9ef8b938c316f00e67c5263033de2f273
Eldest Pasirula <aknutman@gmail.com>
2023-01-09 21:47:05 +0700

    component of login, register, quotation, po, prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './routed-pages/login/login.component';
+import { RegisterComponent } from './routed-pages/register/register.component';
+import { QuotationComponent } from './routed-pages/sales/transaction/quotation/quotation.component';
+import { PurchaseOrderComponent } from './routed-pages/sales/transaction/purchase-order/purchase-order.component';
+import { ProductComponent } from './routed-pages/sales/master/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    QuotationComponent,
+    PurchaseOrderComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
