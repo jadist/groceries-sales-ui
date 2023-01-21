ebd997316ee1ea6d51ad530f0fd220356031544f
Eldest Pasirula <aknutman@gmail.com>
2023-01-22 05:08:22 +0700

    forgot password

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RoutingEnum } from '../../app/models/routing.enum';
 
 import { LoginComponent } from '../../app/pages/login/login.component';
 import { RegisterComponent } from '../../app/pages/register/register.component';
+import { ForgotPasswordComponent } from '../../app/pages/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,10 @@ const routes: Routes = [
   {
     path: RoutingEnum.Register,
     component: RegisterComponent,
+  },
+  {
+    path: RoutingEnum.ForgotPassword,
+    component: ForgotPasswordComponent,
   },
 ];
 
--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  ButtonClickModel,
+  ComponentEntryInputModel,
+} from 'src/app/components/main/entry/entry.model';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.css'],
 })
 export class ForgotPasswordComponent {
+  ForgotPasswordComponentConfig: ComponentEntryInputModel = {
+    Title: 'Password Recovery',
+    SubTitle: '',
+    UsePassword: false,
+    UseRememberMe: false,
+    ButtonText: 'Send Email',
+    Help: {
+      ForgotPassword: false,
+      SignIn: true,
+      SignUp: false,
+    },
+  };
 
+  onButtonClickResetPassword(value: ButtonClickModel) {
+    // this.authService
+    //   .ResetPassword(value.Email!)
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  }
 }
