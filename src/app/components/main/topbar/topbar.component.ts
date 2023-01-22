81eea2b858f4248dc6afd775996f2191b3b203b4
Eldest Pasirula <aknutman@gmail.com>
2023-01-22 16:33:20 +0700

    home

--- src/app/components/main/topbar/topbar.component.ts
+++ src/app/components/main/topbar/topbar.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ToolbarInputModel } from './topbar.model';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
 })
 export class TopbarComponent {
-
+  @Input() ToolbarInputData: ToolbarInputModel = {
+    ToolbarTitle: 'DEFAULT_TITLE',
+  };
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ToolbarInputModel } from '../../components/main/topbar/topbar.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  toolbarInputData: ToolbarInputModel = {
+    ToolbarTitle: 'Home Page',
+  };
+}
