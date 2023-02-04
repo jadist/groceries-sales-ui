677283ee997d378e213bdc0b07debe64a76521c3
Eldest Pasirula <aknutman@gmail.com>
2023-02-04 22:39:26 +0700

    rename db ref

--- src/app/services/firebase/firestore/user/user-role/user-role.service.ts
+++ src/app/services/firebase/firestore/user/user-role/user-role.service.ts
@@ -13,21 +13,21 @@ import { QueryDocumentSnapshot } from '@angular/fire/compat/firestore';
 export class UserRoleService {
   private dbPath = '/USER-ROLE';
 
-  private _userRoleRef: AngularFirestoreCollection<any>;
+  private _dbRef: AngularFirestoreCollection<any>;
 
   constructor(private db: AngularFirestore) {
-    this._userRoleRef = db.collection(this.dbPath);
+    this._dbRef = db.collection(this.dbPath);
   }
 
   getAll(): AngularFirestoreCollection<any> {
-    return this._userRoleRef!;
+    return this._dbRef!;
   }
 
   async getPart(
     startIndex: number,
     rowPerPage: number
   ): Promise<[QueryDocumentSnapshot<any>[], number]> {
-    const totalRowCount = (await this._userRoleRef.ref.get()).size;
+    const totalRowCount = (await this._dbRef.ref.get()).size;
 
     const skippedRow = startIndex * rowPerPage;
 
@@ -35,30 +35,30 @@ export class UserRoleService {
     const first =
       startIndex === 0
         ? undefined
-        : await this._userRoleRef.ref.limit(skippedRow).get();
+        : await this._dbRef.ref.limit(skippedRow).get();
 
     const lastDoc =
       startIndex === 0 ? undefined : first?.docs[first.docs.length - 1];
 
     const nextDoc =
       startIndex === 0
-        ? this._userRoleRef.ref.limit(rowPerPage)
-        : this._userRoleRef.ref.startAfter(lastDoc).limit(rowPerPage);
+        ? this._dbRef.ref.limit(rowPerPage)
+        : this._dbRef.ref.startAfter(lastDoc).limit(rowPerPage);
 
     return [(await nextDoc.get()).docs, totalRowCount];
   }
 
   create(userRoleData: any) {
-    return this._userRoleRef.add({
+    return this._dbRef.add({
       ...userRoleData,
     });
   }
 
   update(id: string, data: any): Promise<void> {
-    return this._userRoleRef.doc(id).update(data);
+    return this._dbRef.doc(id).update(data);
   }
 
   delete(id: string): Promise<void> {
-    return this._userRoleRef.doc(id).delete();
+    return this._dbRef.doc(id).delete();
   }
 }
