982e9d147bac583eea8521e86b3fd5aed5013be2
Eldest Pasirula <aknutman@gmail.com>
2023-02-26 23:43:22 +0700

    refactor sidebar model

--- src/app/components/main/sidebar/sidebar.model.ts
+++ src/app/components/main/sidebar/sidebar.model.ts
@@ -1,16 +1,13 @@
-export interface MenuItem {
+interface Item {
   Icon: string;
   Name: string;
   Value: string;
   Description: string;
   UrlSlug: string[];
-  Child?: {
-    Icon: string;
-    Name: string;
-    Value: string;
-    Description: string;
-    UrlSlug: string[];
-  }[];
+}
+
+export interface MenuItem extends Item {
+  Child?: Item[];
 }
 
 export interface SidebarModel {
